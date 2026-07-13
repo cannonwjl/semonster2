03ec811215adf39c00dc08fa71297ed86ad06c6a
nesimu <ajgdpjp666@gmail.com>
2026-07-13 10:56:24 +0900

    matsuda Monsterに進化処理を追加

--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -1,24 +1,37 @@
 package org.example;
 
 public class Monster {
-  private static final String[] MONSTERS = {
-      "スライム",
-      "サハギン",
-      "ドラゴン",
-      "デュラハン",
-      "シーサーペント"
-  };
+    private static final String[] MONSTERS = {
+        "スライム",
+        "サハギン",
+        "ドラゴン",
+        "デュラハン",
+        "シーサーペント"
+    };
 
-  private final String name;
-  private final int rare;
+    private static final String[] EVOLVED_MONSTERS = {
+        "キングスライム",
+        "サハギンロード",
+        "エンシェントドラゴン",
+        "デュラハンロード",
+        "リヴァイアサン"
+    };
 
-  Monster(int nameNumber, int rareNumber) {
-    name = MONSTERS[nameNumber];
-    rare = rareNumber;
-  }
+    private final String name;
+    private final int rare;
 
-  @Override
-  public String toString() {
-    return name + ":レア度[" + rare + "]\n";
-  }
+    Monster(int nameNumber, int rareNumber) {
+        this.rare = rareNumber;
+
+        if (rareNumber >= 3) {
+            this.name = EVOLVED_MONSTERS[nameNumber];
+        } else {
+            this.name = MONSTERS[nameNumber];
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + ":レア度[" + rare + "]\n";
+    }
 }
