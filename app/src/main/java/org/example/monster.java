1a56e8bc2ea3ba429eef8b0ceb555ffb80bc8b3e
Furusawa Keiji <m1m02630138@oit.ac.jp>
2026-07-24 14:31:26 +0900

    hurusawa monster8

--- app/src/main/java/org/example/monster.java
+++ app/src/main/java/org/example/monster.java
@@ -1,5 +1,27 @@
 package org.example;
 
-public class monster {
+public class Monster {
+  private static final String[] MONSTERS = {
+      "スライム",
+      "サハギン",
+      "ドラゴン",
+      "デュラハン",
+      "シーサーペント",
+      "スズキ",
+      "フェニックス",
+      "ユニコーン"
+  };
 
+  private final String name;
+  private final int rare;
+
+  Monster(int nameNumber, int rareNumber) {
+    name = MONSTERS[nameNumber];
+    rare = rareNumber;
+  }
+
+  @Override
+  public String toString() {
+    return name + ":レア度[" + rare + "]\n";
+  }
 }
